ec6de7b67b106a45b7522a0037440422abf3eb57
dalchand123 <dalchandsharma12@gmail.com>
2017-08-26 11:10:10 +0530

    changes in package.json

--- config/database.js
+++ config/database.js
@@ -1,4 +1,10 @@
-module.exports={
-    database2: 'mongodb://localhost:27017/testing',
-    secret: 'mysecret'
+//  module.exports={
+//      database2: 'mongodb://localhost:27017/testing',
+//      secret: 'mysecret'
+// }
+//provide a sensible default for local development
+database2 = 'mongodb://127.0.0.1:27017/testing';
+//take advantage of openshift env vars when available:
+if(process.env.database2){
+  mongodb_connection_string = process.env.database2 + testing;
 }
--- server.js
+++ server.js
@@ -23,11 +23,18 @@ app.use('/users',users);
 app.get('/',(req, res)=>{
     res.send("Invalid endpoint");
 });
-const port =3000;
-app.listen(port, (err) =>{
-    if(err)
-    console.log("server has some problem");
-    else
-    console.log("server starts at port 3000");
+// const port =3000;
+// app.listen(port, (err) =>{
+//     if(err)
+//     console.log("server has some problem");
+//     else
+//     console.log("server starts at port 3000");
 
+// });
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
